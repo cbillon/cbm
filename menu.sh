b51e7c68e01c1392d1e079ecb22d7b2db689a303
Claude Billon <claude.billon@gmail.com>
2025-05-03 07:53:56 +0000

    add menu translation

---
+++ menu.sh
@@ -0,0 +1,12 @@
+declare -a menu=(
+    [0]="Importing a plugin"
+    [1]="List of plugins (cache)" 
+    [2]="Editing project plugins" 
+    [3]="Adding a plugin to the project" 
+    [4]="Updating the Moodle cache" 
+    [5]="Updating the local plugin repository cache" 
+    [6]="Updating the project codebase" 
+    [7]="Generating a new version of the project" 
+    [8]="Help"
+    [9]="Exit"
+)
