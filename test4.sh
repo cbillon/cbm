13310cdb58f231b01c0efa7938aae8a11af7fbd5
Claude Billon <claude.billon@gmail.com>
2026-01-04 14:48:58 +0100

    20260104 14H48

--- test4.sh
+++ test4.sh
@@ -4,9 +4,64 @@ source includes/env.cnf
 source includes/functions.cfg
 source includes/bash_strict.sh
 
+function is_moodleversion_supported () {  
+  # $1 PLUGIN
+  # $2 MOODLE_VERSION
+  # out if PLUGIN supports MOODLE_VERSION
+  # VCS_URL
+  # DOWNLOAD_URL
+  Start "$*"  
+  local error vcs download 
+  error=0
+  PLUGIN="$1"
+  MOODLE_VERSION="$2"
+
+  VCS_URL=null
+  DOWNLOAD_URL=null
+  
+  jq -r --arg plugin  "$PLUGIN" '.plugins|map(select(.component == $plugin)) | .[]' "$RACINE"/pluglist.json > "$RACINE"/tmp.json
+  if [[ $(jq '.id' "$RACINE"/tmp.json) ]]; then 
+    VCS_URL=$(jq -r '.source' "$RACINE"/tmp.json)
+    [ "$DEBUG" = true ]&& info MOODLE_VERSION: "$MOODLE_VERSION"
+    PLUGIN_MOODLE_VERSION=$(jq -r '[.versions[]| {(.version) : (.supportedmoodles[].release)} ]' "$RACINE"/tmp.json | grep -E ': '"$MOODLE_VERSION" | sort -nr | grep -E -o  "([0-9]{10})" | cut -f 1 -d " ") || true
+    [ "$DEBUG" = true ]&& info PLUGIN_MOODLE_VERSION "$PLUGIN_MOODLE_VERSION"
+    [[ -n "$PLUGIN_MOODLE_VERSION" ]] && success "$PLUGIN": "$MOODLE_VERSION" supported || { error=1; error "$PLUGIN" not support Moodle version "$MOODLE_VERSION"; }
+    vcs=$(jq -r '.versions[]|{(.supportedmoodles[].release) : .vcsrepositoryurl}' "$RACINE"/tmp.json | grep "$2" | head -n 1) || true
+    if [[ "$vcs" =~ ^.*' "'(.*)'"'$ ]]; then
+      VCS_URL="${BASH_REMATCH[1]}"
+    else
+      VCS_URL=null
+      error no value for vcs url
+      error=1
+    fi
+    download=$(jq -r '.versions[]|{(.supportedmoodles[].release) : .downloadurl}' "$RACINE"/tmp.json | grep ': '"$MOODLE_VERSION") || true    
+    if [[ "$download" =~ ^.*' "'(.*)'"'$ ]]; then
+      DOWNLOAD_URL="${BASH_REMATCH[1]}"
+    else
+      DOWNLOAD_URL=null
+      error no value for download url
+      error=1
+    fi
+  else
+    error "$PLUGIN"  not in official repository
+    error=1 
+  fi
+    
+  [ "$DEBUG" = true ] && info vcs "$vcs" download: "$download"
+  
+  [ "$DEBUG" = true ] && info VCS URL: "$VCS_URL" DOWNLOAD URL: "$DOWNLOAD_URL"
+  
+  End
+  return
+}
+
 PLUGIN="$1"
 DEBUG="${2:-false}"
+
+info PLUGIN: "$PLUGIN" DEBUG: "$DEBUG"
 
 jq -r --arg plugin  "$PLUGIN" '.plugins|map(select(.component == $plugin)) | .[]' "$RACINE"/pluglist.json > "$RACINE"/tmp.json
 VCS_URL=$(jq -r '.source' "$RACINE"/tmp.json)
 info VCS_URL: "$VCS_URL"
+
+is_moodleversion_supported "$PLUGIN" '5.0'
--- test5.sh
+++ test5.sh
@@ -4,56 +4,35 @@ source includes/env.cnf
 source includes/functions.cfg
 source includes/bash_strict.sh
 
-PLUGIN="$1"
-MOODLE_VERSION="$2"
-DEBUG="${3:-false}"
+PROJECT="${1:-demo}"
+DEBUG="${2:-false}"
 
+info plugin: "$PROJECT" debug: "$DEBUG"
 
-function is_moodleversion_supported () {  
-  # $1 PLUGIN
-  # $2 MOODLE_VERSION
-  # out if PLUGIN supports MOODLE_VERSION
-  # VCS_URL
-  # DOWNLOAD_URL
-  Start "$*"  
-  local error vcs download 
-  error=0
-  PLUGIN="$1"
-  MOODLE_VERSION="$2"
+function rm_plugin () {
+  Start "$*"
+  local i list
+  
+  get_plugins "$PROJECT"
+  
+  i=0
+  list=''
+  for PLUGIN in "${PLUGINS[@]}"
+  do
+    info plugin: "$PLUGIN"
+    list="$list $i $PLUGIN OFF"
+    ((++i))
+  done
 
-  VCS_URL=null
-  DOWNLOAD_URL=null
-  
-  jq -r --arg plugin  "$PLUGIN" '.plugins|map(select(.component == $plugin)) | .[]' "$RACINE"/pluglist.json > "$RACINE"/tmp.json
-  if [[ $(jq '.id' "$RACINE"/tmp.json) ]]; then 
-    VCS_URL=$(jq -r '.source' "$RACINE"/tmp.json)
-    PLUGIN_MOODLE_VERSION=$(jq -r '[.versions[]| {(.version) : (.supportedmoodles[].release)} ]' "$RACINE"/tmp.json | grep "$MOODLE_VERSION" | sort -nr | grep -E -o  "([0-9]{10})" | cut -f 1 -d " ") || true
-    [[ -n "$PLUGIN_MOODLE_VERSION" ]] && success "$PLUGIN": "$MOODLE_VERSION" supported || { error=1; error "$PLUGIN" not support Moodle version "$MOODLE_VERSION"; }
-    vcs=$(jq -r '.versions[]|{(.supportedmoodles[].release) : .vcsrepositoryurl}' ./tmp.json | grep "$2" | head -n 1) || true
-    [[ -n "$vcs" ]] && VCS_URL="$vcs"
-    download=$(jq -r '.versions[]|{(.supportedmoodles[].release) : .downloadurl}' ./tmp.json | grep "$2") || true    
-    if [[ "$download" =~ ^.*' '(.*)$ ]]; then
-      DOWNLOAD_URL="${BASH_REMATCH[1]}"
-    else
-      DOWNLOAD_URL=null
-      error no value for download url
-      error=1
-    fi
-  else
-    error "$PLUGIN"  not in official repository
-    error=1 
-  fi
-    
-  [ "$DEBUG" = true ] && info vcs "$vcs" download: "$download"
-  
-  info VCS URL: "$VCS_URL" DOWNLOAD URL: "$DOWNLOAD_URL"
-  
+  n=$(menu --title "Plugins cache" --radiolist "Plugin's List" 25 78 16 $list)
+   
+  PLUGIN=$(jq -r ".plugins[$n].name" "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json)
+  jq "del(.plugins[$n])|." "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json > "$PROJECT".tmp && mv "$PROJECT".tmp "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json
+
+  suppress_plugin "$PLUGIN"
+
   End
-  return
 }
 
-
-info plugin: "$PLUGIN" debug: "$DEBUG"
-
-is_moodleversion_supported "$PLUGIN" "$MOODLE_VERSION"
-
+get_project_conf "$PROJECT"
+rm_plugin "$@"
