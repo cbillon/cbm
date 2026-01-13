38679f1ab890f1a574e4088bbf6052c458fab85a
Claude Billon <claude.billon@gmail.com>
2026-01-13 19:09:48 +0100

    20260113

--- test5.sh
+++ test5.sh
@@ -5,34 +5,14 @@ source includes/functions.cfg
 source includes/bash_strict.sh
 
 PROJECT="${1:-demo}"
-DEBUG="${2:-false}"
+PLUGIN="$2"
+DEBUG="${3:-false}"
 
-info plugin: "$PROJECT" debug: "$DEBUG"
-
-function rm_plugin () {
-  Start "$*"
-  local i list
-  
-  get_plugins "$PROJECT"
-  
-  i=0
-  list=''
-  for PLUGIN in "${PLUGINS[@]}"
-  do
-    info plugin: "$PLUGIN"
-    list="$list $i $PLUGIN OFF"
-    ((++i))
-  done
-
-  n=$(menu --title "Plugins cache" --radiolist "Plugin's List" 25 78 16 $list)
-   
-  PLUGIN=$(jq -r ".plugins[$n].name" "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json)
-  jq "del(.plugins[$n])|." "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json > "$PROJECT".tmp && mv "$PROJECT".tmp "$PROJECTS_PATH"/"$PROJECT"/"$PROJECT".json
-
-  suppress_plugin "$PLUGIN"
-
-  End
-}
+info PROJECT: "$PROJECT" PLUGIN: "$PLUGIN" debug: "$DEBUG"
 
 get_project_conf "$PROJECT"
-rm_plugin "$@"
+is_official_plugin "$PLUGIN"
+get_plugin_desired_state "$PLUGIN" "$MOODLE_VERSION"
+
+info PLUGIN: "$PLUGIN" "$PLUGIN_STATE_TYPE" : "$PLUGIN_DESIRED_STATE"
+install_plugin_project "$PLUGIN" "$PLUGIN_STATE_TYPE" "$PLUGIN_DESIRED_STATE"
