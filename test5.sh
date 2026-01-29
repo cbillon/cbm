33c7d5fd5d429b5f7415f442f700cbce0025bde2
Claude Billon <claude.billon@gmail.com>
2026-01-29 11:52:49 +0100

    corr moodle update

--- test5.sh
+++ test5.sh
@@ -5,14 +5,12 @@ source includes/functions.cfg
 source includes/bash_strict.sh
 
 PROJECT="${1:-demo}"
-PLUGIN="$2"
+PLUGIN="${2:-tool_redis}"
 DEBUG="${3:-false}"
 
-info PROJECT: "$PROJECT" PLUGIN: "$PLUGIN" debug: "$DEBUG"
+info PROJECT: "$PROJECT" debug: "$DEBUG"
 
 get_project_conf "$PROJECT"
-is_official_plugin "$PLUGIN"
-get_plugin_desired_state "$PLUGIN" "$MOODLE_VERSION"
 
-info PLUGIN: "$PLUGIN" "$PLUGIN_STATE_TYPE" : "$PLUGIN_DESIRED_STATE"
-install_plugin_project "$PLUGIN" "$PLUGIN_STATE_TYPE" "$PLUGIN_DESIRED_STATE"
+get_plugin_project_state "$PLUGIN" "$MOODLE_VERSION" || get_plugin_default_state "$PLUGIN" "$MOODLE_VERSION"
+
